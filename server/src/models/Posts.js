b62eeafd7c81d7dc02f6ea91542292e3a7d7f932
Felipe <felipesgoncalvess@gmail.com>
2020-12-01 08:21:06 -0300

    refactor: refactor models

--- server/src/models/Posts.js
+++
@@ -1,9 +0,0 @@
-import mongoose, { Schema, model } from 'mongoose';
-
-const PostSchema = new Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'user',
-  },
-});
-export default new model('Post', PostSchema);
--- server/src/models/Profile.js
+++
@@ -1,9 +0,0 @@
-import mongoose, { Schema, model } from 'mongoose';
-
-const ProfileSchema = new Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'user',
-  },
-});
-export default new model('Profile', ProfileSchema);
